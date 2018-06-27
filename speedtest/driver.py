5d13f48556ae3c47745edc1d30bbe6bde5eba3df
Evan Lezar <evanlezar@gmail.com>
2018-06-27 17:57:24 +0200

    Call fluent from python

--- speedtest/driver.py
+++ speedtest/driver.py
@@ -1,11 +1,19 @@
 import argparse
-import fluent
 import json
+import logging
 import subprocess
+import time
 import typing
 
 
-def get_logger(tag: str) -> fluent.sender.FluentSender:
+from fluent import sender
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+def get_logger(tag: str) -> sender.FluentSender:
     try:
         logger = sender.FluentSender(tag)
         yield logger
@@ -13,35 +21,47 @@ def get_logger(tag: str) -> fluent.sender.FluentSender:
         logger.close()
 
 
-def run_speedtest() -> typing.Dict:
+def run_speedtest() -> (typing.Dict, int):
     try:
-        output = subprocess.check_output(["speedtest", "--json"])).decode("utf-8")
+        logger.info("Running speedtest")
+        timestamp = int(time.time())
+        output = subprocess.check_output(["speedtest", "--json"]).decode("utf-8")
         results = json.loads(output)
-        return results
+        logging.debug("results: %s", results)
+        return results, timestamp
     except Exception as e:
-        print("Error running speedtest:", e)
-        return {}
+        logger.error("Error running speedtest: %s", e)
+        return {}, 0
 
-def loop(interval: int):
-    logger = get_logger("speedtest")
+def loop(fs: sender.FluentSender, interval: int):
+    while True:
+        results, timestamp = run_speedtest()
+        if results:
+            if not fs.emit_with_time("laptop", timestamp, results):
+                logger.error("fluent error: %s", fs.last_error)
+                fs.clear_last_error()
 
-
-
-
-
-
-
+        logger.info("Sleeping for %s", interval)
+        time.sleep(interval)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Run speedtest")
     parser.add_argument("--interval", type=int, default=5,
-                        help="The number of minutes to wait between successive speedtest runs")
+                        help="The number of seconds to wait between successive speedtest runs")
+    parser.add_argument("--tag", type=str, default="speedtest")
+    parser.add_argument("--fluent-host", type=str, default="localhost",
+                       help="The hostname where the fluentd service is running")
+    parser.add_argument("--fluent-port", type=int, default=24224,
+                       help="The port where the fluentd service is running")
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
+    with sender.FluentSender(args.tag, host=args.fluent_host, port=args.fluent_port) as fs:
+        loop(fs, args.interval)
+
 
 if __name__ == "__main__":
     main()
